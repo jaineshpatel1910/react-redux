1e83e3f45a295ec9067bdef8f26fa9247341dcb2
jaineshpatel1910 <jaineshpatel.1910@gmail.com>
2022-09-18 11:24:00 +0530

    Created simple redux example

---
+++ react-redux-demo/src/components/CakeContainer.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { buyCake } from '../redux'
+import { connect } from 'react-redux'
+
+const CakeContainer = (props) => {
+    return (
+        <div>
+            <h1>Cakes- {props.numberOfCakes}</h1>
+            <button onClick={props.buyCake}>Buy Cake</button>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        numberOfCakes: state.numberOfCakes
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CakeContainer)
